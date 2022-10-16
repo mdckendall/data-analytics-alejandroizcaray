7a4f55b7537a2d077d0e72eb5856a79488999c4c
Alejandro Izcaray <113472304+alejandroizcaray@users.noreply.github.com>
2022-10-16 20:00:55 +0000

    second submission

--- Main.java
+++ Main.java
@@ -5,46 +5,42 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 class Main {
-    public static void main(String[] args) {
+  public static void main(String[] args){
+    int input = 0;
+    ArrayList<String> arrayList = new ArrayList<>();
+
+    while (input != 5) {
+      System.out.println("Press 1 to learn about salary.");
+      System.out.println("Press 2 to learn about the job.");
+      System.out.println("Press 3 to learn about demand.");
+      System.out.println("Press 4 to learn view current students.");
+      System.out.println("Press 5 to quit.");
+      Scanner scanner = new Scanner(System.in);
+      input = scanner.nextInt();
+      if (input == 1) {
+        System.out.println("$98,345 average salary.");
+      } else if (input == 2) {
+        System.out.println("US News - 100 Best Jobs!");
+      } else if (input == 3) {
+        System.out.println("Top 10 Forbes In-Demand Jobs!");
+      } else if (input == 4) {
         try {
-            int input = 0;
-            ArrayList<String> arrayList = new ArrayList<>();
-            try {
-                FileReader fr = new FileReader("names.txt");
-                Scanner fileScanner = new Scanner(fr);
-                fileScanner.nextLine();
-                for (int i = 0; i < 50; i++) {
-                    arrayList.add(fileScanner.nextLine());
-                }
-            } catch (FileNotFoundException exception) {
-                System.out.println(exception.getMessage());
-            }
-            while (input != 5) {
-                System.out.println("Press 1 to learn about salary.");
-                System.out.println("Press 2 to learn about the job.");
-                System.out.println("Press 3 to learn about demand.");
-                System.out.println("Press 4 to learn view current students.");
-                System.out.println("Press 5 to quit.");
-                Scanner scanner = new Scanner(System.in);
-                input = scanner.nextInt();
-                if (input == 1) {
-                    System.out.println("$98,345 average salary.");
-                } else if (input == 2) {
-                    System.out.println("US News - 100 Best Jobs!");
-                } else if (input == 3) {
-                    System.out.println("Top 10 Forbes In-Demand Jobs!");
-                } else if (input == 4) {
-                    System.out.println("Current Students:");
-                    for (String Is : arrayList) {
-                        System.out.println(Is);
-                    }
-                    if (input == 5) {
-                        break;
-                    }
-                }
-            }
-        } catch (NoSuchElementException ex) {
-            System.out.println(ex.getMessage());
+          FileReader fr = new FileReader("names.txt");
+          Scanner fileScanner = new Scanner(fr);
+          while (fileScanner.hasNextLine()) {
+            arrayList.add(fileScanner.nextLine());
+          }
+        } catch (FileNotFoundException e) {
+          System.out.println(e);
         }
+        System.out.println("Current Students:");
+        for (String Is : arrayList) {
+          System.out.println(Is);
+        }
+        if (input == 5) {
+          break;
+        }
+      }
     }
+  }
 }
